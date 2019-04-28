157c5f7056c0c052514060a75d6d782c8e1b6c8d
lijun <lijun@kedacom.com>
2019-04-29 06:14:29 +0800

    '部分标识符的讲解'

--- one/src/main/java/one/ClassUsageDeclare.java
+++ one/src/main/java/one/ClassUsageDeclare.java
@@ -1,6 +1,7 @@
 package one;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 
 /**
  * Class主动使用与被动使用说明
@@ -8,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class ClassUsageDeclare {
 
     @Test
-    public void tP() {
+    public void testParentStatic() {
         /**
          * 包含被调用静态变量或方法的直接类会被初始化
          * ====输出为====
@@ -20,7 +21,7 @@ public class ClassUsageDeclare {
     }
 
     @Test
-    public void tC() {
+    public void testChildStatic() {
         /**
          * 初始化子类,也意味着父类的主动调用.
          * ====输出为====
@@ -32,7 +33,34 @@ public class ClassUsageDeclare {
         System.out.println(Child.child);
     }
 
+    @Test
+    public void testParentFinal() {
+        /**
+         * static final常量会被存入调用该常量所在方法的类的常量池中，
+         * 即ClassUsageDeclare类中,本质上未主动调用Parent类
+         * 不会使其初始化
+         *  ====输出为====
+         *  hello world
+         *  ========
+         */
+        System.out.println(Child.str);
+    }
 
+    @Test
+    public void testNodeSign() {
+        System.out.println(Constant.INT_SIX);
+        System.out.println(Constant.INT_ONE);
+        System.out.println(Constant.INT_THREE);
+        System.out.println(Constant.INT_TWO);
+        System.out.println(Constant.INT_FOUR);
+        System.out.println(Constant.INT_FIVE);
+        System.out.println(Constant.H_W);
+        System.out.println(Constant.FLOAT_TWO);
+        System.out.println(Constant.BYTE_TWO);
+        System.out.println(Constant.BYTE_THREE);
+        System.out.println(Constant.SHORT_TWO);
+        System.out.println(Constant.SHORT_THREE);
+    }
 }
 
 class Parent {
@@ -41,6 +69,8 @@ class Parent {
     }
 
     protected static int parent = 1;
+    public static final String str = "hello world";
+
 }
 
 class Child extends Parent {
@@ -49,4 +79,21 @@ class Child extends Parent {
     }
 
     protected static int child = 2;
+
+}
+
+class Constant {
+    public static final String H_W = "hello world";
+    public static final int INT_ONE = 1;
+    public static final int INT_TWO = 2;
+    public static final int INT_THREE = 3;
+    public static final int INT_FOUR = 4;
+    public static final int INT_FIVE = 5;
+    public static final int INT_SIX = 6;
+    public static final byte BYTE_TWO = 55;
+    public static final byte BYTE_THREE = 3;
+    public static final float FLOAT_TWO = 2;
+    public static final short SHORT_TWO = 6;
+    public static final short SHORT_THREE= 3;
+
 }
